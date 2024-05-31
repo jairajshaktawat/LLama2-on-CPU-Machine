c7b2d9a8b761eed33bc84ce02ba1ee904254286b
jairajshaktawat <jairajshaktawat@outlook.com>
2024-05-31 16:31:46 -0400

    added requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Generative AI Project",
+    version = '0.0.0',
+    author = 'Jairaj',
+    author_email= 'jairajshaktawat@outlook.com',
+    packages = find_packages(),
+    install_requires=[]
+)
